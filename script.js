6dd682c2af493bbc16ec4bed11f40f911a717057
David Islam <david.islam24@gmail.com>
2022-09-13 15:41:58 -0700

    Create add(), subtract(), multiply(), and divide() functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        console.error("Cannot divide by zero");
+        return;
+    }
+    return a / b;
+}
