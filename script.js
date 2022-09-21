fa65e1cffe95af9856822ac94b102ac60ae066b8
David Islam <david.islam24@gmail.com>
2022-09-21 09:03:37 -0700

    Begin working on making the calculator actually work

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 /* === Globals === */
 let displayValue = '0';
+let firstOperand = '';
+let secondOperand = '';
+let firstOperator = '';
+let secondOperator = '';
+let res = '';
 
 /* === DOM Elements === */
 const displayEle = document.getElementById("display");
 const numberButtons = document.querySelectorAll('.operand');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('.equals');
 
 /* === Functions === */
 function updateDisplay() {
-    // Gotcha: Round answers with long decimals 
+    // Gotcha: Prevent display overflow  
     displayEle.innerText = displayValue.length > 9 ?
                            displayValue.substring(0, 9) :
                            displayValue;
 }
 
-function handleNumberBtnClick(e) {
+function handleNumberBtnClick(e) { // TODO: Investigate logic in Chrome debugger
     const num = e.target.value;
-    if (displayValue === '0') {
-        displayValue = num;
-    } else {
-        displayValue += num;
+    if (!firstOperator) { // Handle input to first operand 
+        if (+displayValue === 0 || displayValue === firstOperand) { 
+            displayValue = num;
+        } else {
+            displayValue += num;
+        }
+    } else { // Handle input to second operand 
+        if (displayValue === firstOperand) {
+            displayValue = num;
+        } else {
+            displayValue += num; 
+        }
     }
     updateDisplay();
+}
+
+function handleOperatorBtnClick(e) {
+    const operator = e.target.value; 
+
+    if (firstOperand && !secondOperand) {
+        // Handle second operator input
+
+    } 
+
+    if (!(firstOperand || secondOperand)) {
+        // Handle first operator input 
+        firstOperator = operator; 
+        firstOperand = displayValue;
+    }
 }
 
 function add(a, b) {
@@ -60,6 +90,7 @@ function operate(operator, a, b) {
 
 /* === Event listeners === */
 numberButtons.forEach(btn => btn.addEventListener('click', handleNumberBtnClick));
+operatorButtons.forEach(btn => btn.addEventListener('click', handleOperatorBtnClick));
 
 /* === Invoke functions === */
 updateDisplay();
